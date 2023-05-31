fb3961b559687c89989fefdf051d74e9f7578201
juliovianadev <juliovianadev@gmail.com>
2023-05-31 19:30:54 -0300

    20:44

---
+++ capputeno/src/components/primary-input.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+const PrimaryInput = styled.input`
+    width: 352px;
+    background-color: var(--bg-secondary);
+    border-radius: 8px;
+    padding: 10px 16px;
+`;
